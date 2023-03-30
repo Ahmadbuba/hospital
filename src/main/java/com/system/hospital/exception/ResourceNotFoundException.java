28c01a7aaee49a2447a57743a0ec146d51f31e15
AhmadBuba <ahmadbubacherryfield@gmail.com>
2023-03-30 14:46:05 +0100

    removal - i removed serialversionUID variable in ResourceNotFoundException class

--- src/main/java/com/system/hospital/exception/ResourceNotFoundException.java
+++ src/main/java/com/system/hospital/exception/ResourceNotFoundException.java
@@ -1,9 +1,7 @@
 package com.system.hospital.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    private static final long serialversionUID = 1;
-
-	public ResourceNotFoundException(String message) {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
