639def72048d0bed312e2085194a80639cdb8fb4
AhmadBuba <ahmadbubacherryfield@gmail.com>
2023-05-20 02:16:32 +0100

    bug related to testing with testcontainers and postgres solved

--- src/test/java/com/system/hospital/repository/PatientRepositoryTest.java
+++ src/test/java/com/system/hospital/repository/PatientRepositoryTest.java
@@ -1,31 +1,18 @@
 package com.system.hospital.repository;
 
-import com.system.hospital.HospitalApplication;
 import com.system.hospital.model.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
-
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PatientRepositoryTest {
+
     @Autowired
     private PatientRepository patientRepository;
 
