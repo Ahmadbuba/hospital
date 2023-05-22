c552bb118f96d7c53b9e403f2c1cf723b6f4b366
AhmadBuba <ahmadbubacherryfield@gmail.com>
2023-05-22 07:52:30 +0100

    utilityTests continued 1

--- src/test/java/com/system/hospital/service/UtilityServiceTest.java
+++ src/test/java/com/system/hospital/service/UtilityServiceTest.java
@@ -1,17 +1,20 @@
 package com.system.hospital.service;
 
 import com.system.hospital.dto.AddressDto;
+import com.system.hospital.dto.PatientDto;
+import com.system.hospital.dto.PatientNextOfKinDto;
 import com.system.hospital.dto.PersonalDetailDto;
-import com.system.hospital.model.Address;
-import com.system.hospital.model.Gender;
-import com.system.hospital.model.Person;
-import com.system.hospital.model.PersonalDetail;
+import com.system.hospital.model.*;
 import org.assertj.core.api.Assertions;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -159,4 +162,131 @@ public class UtilityServiceTest {
 
     }
 
+    @Test
+    public void buildPatientNextOfKinValues() {
+        AddressDto addressDto = AddressDto.builder()
+                .houseNumber(30)
+                .street("Douala")
+                .state("Abuja")
+                .build();
+       PatientNextOfKinDto patientNextOfKinDto = PatientNextOfKinDto.builder()
+                .first_name("James")
+                .last_name("Gana")
+                .gender("Male")
+                .address(addressDto)
+                .build();
+       PatientNextOfKin patientNextOfKin = PatientNextOfKin.builder()
+                .person(Person.builder()
+                        .firstName(patientNextOfKinDto.first_name())
+                        .lastName(patientNextOfKinDto.last_name())
+                        .gender(UtilityService.getGenderEnum(patientNextOfKinDto.gender()))
+                        .build())
+                .address(Address.builder()
+                        .houseNumber(patientNextOfKinDto.address().houseNumber())
+                        .street(patientNextOfKinDto.address().street())
+                        .state(patientNextOfKinDto.address().state())
+                        .build())
+                .build();
+        Assertions.assertThat(UtilityService.buildPatientNextOfKin(patientNextOfKinDto).getPerson().getFirstName()).isEqualTo(patientNextOfKin.getPerson().getFirstName());
+        Assertions.assertThat(UtilityService.buildPatientNextOfKin(patientNextOfKinDto).getAddress()).isNotNull();
+        Assertions.assertThat(UtilityService.buildPatientNextOfKin(patientNextOfKinDto).getAddress().getState()).isEqualTo(patientNextOfKinDto.address().state());
+    }
+
+    @Test
+    public void buildPatientNextOfKinValueReturnsNullForAddressIfAddressNotProvided() {
+        PatientNextOfKinDto patientNextOfKinDto = PatientNextOfKinDto.builder()
+                .first_name("James")
+                .last_name("Gana")
+                .gender("Male")
+                .build();
+        Assertions.assertThat(UtilityService.buildPatientNextOfKin(patientNextOfKinDto).getAddress()).isNull();
+    }
+
+    @Test
+    public void returnNextOfKins() {
+        Assertions.assertThat(UtilityService.returnNextOfKins(Optional.ofNullable(null)).size()).isNotNull();
+        Assertions.assertThat(UtilityService.returnNextOfKins(Optional.ofNullable(null)).size()).isEqualTo(0);
+    }
+
+    @Test
+    public void returnNextOfKins2() {
+        List<PatientNextOfKinDto> theNextOfKinDtos = new ArrayList<>();
+        PatientNextOfKinDto patientNextOfKinDto = PatientNextOfKinDto.builder()
+                .first_name("James")
+                .last_name("Gana")
+                .gender("Male")
+                .build();
+        theNextOfKinDtos.add(patientNextOfKinDto);
+        Assertions.assertThat(UtilityService.returnNextOfKins(Optional.ofNullable(theNextOfKinDtos)).size()).isEqualTo(1);
+        Assertions.assertThat(UtilityService.returnNextOfKins(Optional.ofNullable(theNextOfKinDtos)).get(0).getAddress()).isNull();
+        Assertions.assertThat(UtilityService.returnNextOfKins(Optional.ofNullable(theNextOfKinDtos)).get(0).getPerson().getFirstName()).isEqualTo(patientNextOfKinDto.first_name());
+    }
+
+    @Test
+    public void convertPatientDtoToPatient() {
+        AddressDto addressDto = AddressDto.builder()
+                .houseNumber(30)
+                .street("Douala")
+                .state("Abuja")
+                .build();
+        PatientDto patientDto = PatientDto.builder()
+                .first_name("Alfaki")
+                .last_name("Isa")
+                .gender("Male")
+                .address(addressDto)
+                .build();
+        Assertions.assertThat(UtilityService.convertPatientDtoToPatient(patientDto).getPersonalDetail()).isNull();
+        Assertions.assertThat(UtilityService.convertPatientDtoToPatient(patientDto).getPatientNextOfKinList().size()).isEqualTo(0);
+        Assertions.assertThat(UtilityService.convertPatientDtoToPatient(patientDto).getRecords()).isNull();
+    }
+
+    @Test
+    public void convertPatientDtoToPatient2() {
+        List<PatientNextOfKinDto> theNextOfKinDtos = new ArrayList<>();
+        PatientNextOfKinDto patientNextOfKinDto = PatientNextOfKinDto.builder()
+                .first_name("James")
+                .last_name("Gana")
+                .gender("Male")
+                .build();
+        theNextOfKinDtos.add(patientNextOfKinDto);
+        AddressDto addressDto = AddressDto.builder()
+                .houseNumber(30)
+                .street("Douala")
+                .state("Abuja")
+                .build();
+        PatientDto patientDto = PatientDto.builder()
+                .first_name("Alfaki")
+                .last_name("Isa")
+                .gender("Male")
+                .address(addressDto)
+                .next_of_kins(theNextOfKinDtos)
+                .build();
+
+        Assertions.assertThat(UtilityService.convertPatientDtoToPatient(patientDto).getPatientNextOfKinList().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void convertFromPatientToPatientResponse() {
+        Patient patient = Patient.builder()
+                .id(1)
+                .person(Person.builder()
+                        .firstName("Fatima")
+                        .lastName("Buba")
+                        .gender(Gender.FEMALE)
+                        .build())
+                .address(Address.builder()
+                        .id(2)
+                        .houseNumber(12)
+                        .street("Khartoum")
+                        .state("Abuja")
+                        .build())
+                .personalDetail(PersonalDetail.builder()
+                        .genoType("AS")
+                        .weight(55)
+                        .bloodGroup("O")
+                        .build())
+                .build();
+        Assertions.assertThat(UtilityService.convertFromPatientToPatientResponse(patient).address()).isNotNull();
+        Assertions.assertThat(UtilityService.convertFromPatientToPatientResponse(patient).next_of_kins().size()).isEqualTo(0);
+    }
 }
